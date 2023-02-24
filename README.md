3ed314e3c7df90d015d901054946cd28b1914f66
ranya <ranya@meiqia.com>
2023-02-25 00:20:11 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,31 @@
+<a href="compiler.go"><img width="731" alt="THE SUPER TINY COMPILER" src="https://cloud.githubusercontent.com/assets/952783/14413766/134c4068-ff39-11e5-996e-9452973299c2.png"/></a>
+
+Inspired by [The Super Tiny Compiler][1] project. I rewrite it by Golang to make it easier to understand, and I also reduce some concept!
+
+### Usage
+
+```
+$ git clone git@github.com:ClarkThan/the-super-super-tiny-compiler.git
+$ cd the-super-super-tiny-compiler && go build compiler.go
+
+$ ./compiler
+add(3, sub(4, len("foo")));
+
+$ ./compiler 23
+23;
+
+$ ./compiler '(add 23 (len "Jordan"))'
+add(23, len("Jordan"));
+```
+
+
+### Funny
+
+Feel free to modify code for inspecting the Tokenizer, Parser and CodeGen output.
+And I also encourage you improve the error message of the invalid input.
+
+---
+
+[![cc-by-4.0](https://licensebuttons.net/l/by/4.0/80x15.png)](http://creativecommons.org/licenses/by/4.0/)
+
+[1]: https://github.com/thejameskyle/the-super-tiny-compiler
---
+++ compiler.go
@@ -0,0 +1,221 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+const (
+	Name       string = "Name"
+	Paren      string = "Paren"
+	Number     string = "Number"
+	String     string = "String"
+	Expression string = "Expression"
+	Program    string = "Program"
+)
+
+// data structure for Tokenizer phrase
+type Token struct {
+	typ, val string
+}
+
+// data structure for Parser and CodeGen phrase
+type Node interface {
+	Type() string
+}
+
+type Literal struct {
+	typ, val string
+}
+
+func (n Literal) Type() string {
+	return n.typ
+}
+
+type Expr struct {
+	typ, name string
+	params    []Node
+}
+
+func (n Expr) Type() string {
+	return n.typ
+}
+
+type Ast struct {
+	typ  string
+	body []Node
+}
+
+func (n Ast) Type() string {
+	return n.typ
+}
+
+func main() {
+	src := `(add 3 (sub 4 (len "foo")))` // add(3, sub(4, len("foo")));
+	if len(os.Args) > 1 {
+		src = os.Args[1]
+	}
+	code := Compile(src)
+	fmt.Println(code)
+}
+
+func Compile(src string) string {
+	tokens := Tokenizer(src)
+	// fmt.Println(tokens)
+	// [{Paren (} {Name add} {Number 3} {Paren (} {Name sub} {Number 4} {Paren (} {Name len} {String foo} {Paren )} {Paren )} {Paren )}]
+	ast := Parser(tokens)
+	// fmt.Println(ast)
+	// {Program [
+	// 		{Expression add [
+	// 			{Number 3}
+	// 			{Expression sub [
+	// 				{Number 4}
+	// 				{Expression len [
+	// 					{String foo}
+	// 				]}
+	// 			]}
+	// 		]}
+	// ]}
+	code := CodeGen(ast)
+	return code
+}
+
+func Tokenizer(src string) (tokens []Token) {
+	if len(src) == 0 {
+		return
+	}
+
+	chars := []rune(src)
+	idx, length := 0, len(chars)
+	chars = append(chars, '\n')
+	for idx < length {
+		c := chars[idx]
+		if c == '(' || c == ')' {
+			tokens = append(tokens, Token{Paren, string(c)})
+			idx++
+			continue
+		}
+		if isNum(c) {
+			numIdx := idx + 1
+			for c = chars[numIdx]; isNum(c); c = chars[numIdx] {
+				numIdx++
+			}
+			tokens = append(tokens, Token{Number, string(chars[idx:numIdx])})
+			idx = numIdx
+			continue
+		}
+		if isSpace(c) {
+			idx++
+			for c = chars[idx]; isSpace(c); c = chars[idx] {
+				idx++
+			}
+			continue
+		}
+		if c == '"' {
+			strIdx := idx + 1
+			for c = chars[strIdx]; c != '"'; c = chars[strIdx] {
+				strIdx++
+			}
+			tokens = append(tokens, Token{String, string(chars[idx+1 : strIdx])})
+			idx = strIdx + 1 // skip the right `"`
+			continue
+		}
+		if isAlpha(c) {
+			alphaIdx := idx + 1
+			for c = chars[alphaIdx]; isAlpha(c); c = chars[alphaIdx] {
+				alphaIdx++
+			}
+			tokens = append(tokens, Token{Name, string(chars[idx:alphaIdx])})
+			idx = alphaIdx
+			continue
+		}
+		panic(fmt.Sprintf(`Unknown char[%d]: %c`, idx, c))
+	}
+
+	return
+}
+
+func Parser(tokens []Token) Ast {
+	tracer := &Tracer{
+		tokens: tokens,
+	}
+	return Ast{
+		typ:  Program,
+		body: []Node{tracer.walk()},
+	}
+}
+
+func CodeGen(n Node) (code string) {
+	switch n.Type() {
+	case Number:
+		n := n.(Literal)
+		return n.val
+	case String:
+		n := n.(Literal)
+		return fmt.Sprintf("%q", n.val)
+	case Expression:
+		n := n.(Expr)
+		var args []string
+		for _, para := range n.params {
+			args = append(args, CodeGen(para))
+		}
+		return fmt.Sprintf("%s(%s)", n.name, strings.Join(args, ", "))
+	case Program:
+		n := n.(Ast)
+		var lines []string
+		for _, b := range n.body {
+			lines = append(lines, CodeGen(b)+";")
+		}
+		return strings.Join(lines, "\n")
+	default:
+		return ""
+	}
+}
+
+type Tracer struct {
+	tokens []Token
+	idx    int
+}
+
+func (t *Tracer) walk() Node {
+	if t.idx >= len(t.tokens) {
+		panic("error for parsing")
+	}
+	tok := t.tokens[t.idx]
+	if tok.typ == Number || tok.typ == String {
+		t.idx++
+		return Literal{tok.typ, tok.val}
+	}
+	if tok.val == "(" {
+		t.idx += 2
+		expr := Expr{
+			typ:  Expression,
+			name: t.tokens[t.idx-1].val,
+		}
+		for tok = t.tokens[t.idx]; tok.val != ")"; tok = t.tokens[t.idx] {
+			expr.params = append(expr.params, t.walk())
+		}
+		t.idx++ // skip the closing parentheses: `)`
+		return expr
+	}
+	panic(fmt.Sprintf("Syntax error - token[%d]: %s", t.idx, tok))
+}
+
+func isNum(c rune) bool {
+	if c >= '0' && c <= '9' {
+		return true
+	}
+	return false
+}
+
+func isAlpha(c rune) bool {
+	if (c >= 'a' && c < 'z') || (c >= 'A' && c < 'Z') {
+		return true
+	}
+	return false
+}
+
+func isSpace(c rune) bool {
+	return c == ' ' || c == '\t' || c == '\n'
+}
